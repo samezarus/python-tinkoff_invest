fbc324d4f5ede0082d25efee883c062c5d984a5d
samezarus <Zv1c9xknturyjs>
2021-07-04 13:02:30 +1000

    обновление demon.py

--- demon.py
+++ demon.py
@@ -5,7 +5,6 @@
 """
 
 import class_tinkoff_invest
-import os
 import sys
 
 if __name__ == '__main__':
@@ -15,17 +14,18 @@ if __name__ == '__main__':
     if len(sys.argv) > 1:
         ext_cmd = sys.argv[1]
 
+    """
     # Очищение лога
     try:
         os.remove('log.txt')
-    except:
+    except IndexError:
         pass
+    """
 
     # Создание экземпляра класса для работы с инвестициями
     ti = class_tinkoff_invest.TinkoffInvest('conf.txt')
 
     # Свечи в mysql с интервалом 1 минута
-    #ext_cmd = '-candles_to_mysql_1min'
+    ext_cmd = '-candles_to_mysql_1min'
     if ext_cmd == '-candles_to_mysql_1min':
         ti.figis_candles_history_to_mysql('1min')
-
